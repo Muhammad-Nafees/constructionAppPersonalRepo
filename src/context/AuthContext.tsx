2f3c936def5d5f120f2357dea1d30d7aa8c03126
Muhammad Nafees <nafeesmac@Muhammads-Air.bbrouter>
2025-07-10 03:01:40 +0500

    addincentives-rafactor

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -58,7 +58,6 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
     );
 };
 
-
 export const useAuth = (): AuthContextType => {
     const context = useContext(AuthContext);
     if (!context) {
